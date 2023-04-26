8a5be826f7cea20d7a1d2d128da166969184cdf1
leolanese <laneseleo@gmail.com>
2023-04-26 16:52:01 +0100

    udpating styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,14 +5,13 @@ const Card = ({ name, email, id }) => {
     // const { name, emial, id } = props;
     return (
         <>
-          <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-            <img src={`https://robohash.org/${id}?50x50`} alt='card picture' />
-            <div>
-                <h2>{ name } </h2>
-                <p>{ email }</p>
-            </div>
+          <article className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5 ">
+          <div className="tc">
+            <img src={`https://robohash.org/${id}?size=75x75`} className="br-100 h4 w4 dib ba b--black-05 pa2" title="Photo of a kitty staring at you" />
+            <h1 className="f3 mb2">{ name }</h1>
+            <h2 className="f5 fw4 gray mt0">{ email }</h2>
           </div>
-
+        </article>
         </>
     );
 }
