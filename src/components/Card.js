af19e967b1b0be215b4230382d0bf1dd88ecba9f
Leo Lanese <laneseleo@gmail.com>
2023-05-10 21:02:56 +0100

    updating Search component styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,14 +6,14 @@ const Card = ({ name, email, id }) => {
     return (
         <>
           <article className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5 ">
-          <div className="tc">
-            <img src={`https://placebear.com/200/20${id}`}
-              className="br-100 h4 w4 dib ba b--black-05 pa2" 
-              title={`a lovely bear called: ${name}`} 
-              alt="card person"/>
-            <h1 className="f3 mb2">{ name }</h1>
-            <h2 className="f5 fw4 gray mt0">{ email }</h2>
-          </div>
+            <div className="tc">
+              <img src={`https://placebear.com/200/20${id}`}
+                className="br-100 h4 w4 dib ba b--black-05 pa2" 
+                title={`a lovely bear called: ${name}`} 
+                alt="card person"/>
+              <h1 className="f3 mb2">{ name }</h1>
+              <h2 className="f5 fw4 gray mt0">{ email }</h2>
+            </div>
         </article>
         </>
     );
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,8 +2,13 @@ import React, {memo} from 'react';
 
 const Search = ({onSearchChange}) => {
     return (
-        <div className="pa2 ba ">
-            <input className="w-90" type='search 'placeholder='Search ...' onChange={onSearchChange} />
+        <div className="pa2 bap-4 border border-gray-200 flex items-center justify-center">
+            <input 
+                className="w-90 w-4/5 p-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-5 focus:ring-blue-500" 
+                type='search' 
+                placeholder='Search ...' 
+                onChange={onSearchChange} 
+            />
         </div>
     );
 }
